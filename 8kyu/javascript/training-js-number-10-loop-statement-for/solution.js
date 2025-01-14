fa6a3ecee61a152503bfd155b9a943d66835a3d9
Thomas Loyan <thomas.loyan@gmail.com>
2025-01-14 15:44:03 +0100

    8kyu/javascript/training-js-number-10-loop-statement-for

---
+++ 8kyu/javascript/training-js-number-10-loop-statement-for/solution.js
@@ -0,0 +1,12 @@
+function pickIt(arr){
+  var odd=[],even=[];
+  
+  for (let i = 0; i<arr.length; i++) {
+    if (arr[i]%2)
+      odd.push(arr[i]);
+    else
+      even.push(arr[i]);
+  }
+  
+  return [odd,even];
+}
