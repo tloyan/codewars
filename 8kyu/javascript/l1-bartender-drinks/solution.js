336406afb2f0d1a8f23deb241fabf939d3205349
Thomas Loyan <thomas.loyan@gmail.com>
2025-01-14 15:45:42 +0100

    8kyu/javascript/l1-bartender-drinks

---
+++ 8kyu/javascript/l1-bartender-drinks/solution.js
@@ -0,0 +1,8 @@
+function getDrinkByProfession(param){
+  return  {"jabroni":"Patron Tequila",
+  "school counselor":"Anything with Alcohol",
+  "programmer" :"Hipster Craft Beer",
+  "bike gang member"	:"Moonshine",
+  "politician":	"Your tax dollars",
+  "rapper":	"Cristal"}[param.toLowerCase()] || "Beer"
+}
