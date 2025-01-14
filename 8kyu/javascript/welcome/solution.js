9c5ed24c3899e3fbe39cc28e5c08553ec1f4cdf9
Thomas Loyan <thomas.loyan@gmail.com>
2025-01-14 15:50:54 +0100

    8kyu/javascript/welcome

---
+++ 8kyu/javascript/welcome/solution.js
@@ -0,0 +1,7 @@
+function greet(language) {
+    return {english: 'Welcome',czech: 'Vitejte',danish: 'Velkomst',
+    dutch: 'Welkom',estonian: 'Tere tulemast',finnish: 'Tervetuloa',
+    flemish: 'Welgekomen',french: 'Bienvenue',german: 'Willkommen',irish: 'Failte',
+    italian: 'Benvenuto',latvian: 'Gaidits',lithuanian: 'Laukiamas',polish: 'Witamy',
+    spanish: 'Bienvenido',swedish: 'Valkommen',welsh: 'Croeso'}[language] || 'Welcome'
+}
